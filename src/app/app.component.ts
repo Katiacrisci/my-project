a28aedc351014fa6de314b1b7652b938fadcbbdf
Katiacrisci <katcrisci@gmail.com>
2023-11-27 16:15:31 +0100

    add bootstrap and first-second-third components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FirstComponentComponent, SecondComponent, ThirdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
